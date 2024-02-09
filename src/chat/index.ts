b6fec898f05665600c20c0d0c3c5ac262ab040ef
taralnagar <taral.nagar@bytestechnolab.com>
2024-02-09 10:57:32 +0530

    file name and file size related change

--- src/chat/index.ts
+++ src/chat/index.ts
@@ -4,7 +4,7 @@ import { verifyToken } from './verifyToken';
 import { SocketEvents } from './socketEvents';
 import { Request, Response } from 'express';
 import * as redis from 'redis';
-import { promisify } from 'util'; 
+import { promisify } from 'util';
 
 const onlineUsers: Record<
   string,
@@ -24,7 +24,7 @@ redisClient.connect().then(() => {
 
 const setAsync = promisify(redisClient.set).bind(redisClient);
 
-export const initChat = async(io: SocketIOServer) => {
+export const initChat = async (io: SocketIOServer) => {
 
   io.on('connection', async (socket: Socket) => {
     console.log('A user connected:', socket.id);
@@ -44,77 +44,79 @@ export const initChat = async(io: SocketIOServer) => {
       io.emit(SocketEvents.UpdateUsers, Object.keys(onlineUsers));
 
       // Store online users in Redis with proper error handling
-      socket.on(SocketEvents.Disconnect, async() => {
+      socket.on(SocketEvents.Disconnect, async () => {
         delete onlineUsers[userId]
         io.emit(SocketEvents.UpdateUsers, Object.keys(onlineUsers));
 
         // Update online users in Redis after disconnection
-          await setAsync('onlineUsers', JSON.stringify(Object.keys(onlineUsers)));
+        await setAsync('onlineUsers', JSON.stringify(Object.keys(onlineUsers)));
       });
 
       socket.on(
         SocketEvents.SendMessage,
-        async (data: { to: string; content: string ,type: MessageType }) => {
+        async (data: { to: string; content: string, file_name: string, file_size: string, type: MessageType }) => {
           console.log('SendMessage event received:', data);
-          const { to, content, type} = data;
-          const message = new Message({ from: userId, to, content,type });
+          const { to, content, type, file_name, file_size } = data;
+          const message = new Message({ from: userId, to, content, type, file_name, file_size });
 
           try {
-          await message.save();
-          // Store message in Redis
-          const messagesss = await redisClient.rPush(`messages:${to}:${userId}`, JSON.stringify(message));
-          const recipientSocket = onlineUsers[to]?.socket;
+            await message.save();
+            // Store message in Redis
+            const messagesss = await redisClient.rPush(`messages:${to}:${userId}`, JSON.stringify(message));
+            const recipientSocket = onlineUsers[to]?.socket;
 
-          if (recipientSocket) {
-            console.log(`Found socket for ${to} -> ${{recipientSocket}}`)
-            recipientSocket.emit(SocketEvents.ReceiveMessage, {
-              from: userId,
-              content,
-              type
-            });
-          }else {
-            console.log('Recipient socket not found');
+            if (recipientSocket) {
+              console.log(`Found socket for ${to} -> ${{ recipientSocket }}`)
+              recipientSocket.emit(SocketEvents.ReceiveMessage, {
+                from: userId,
+                content,
+                file_name,
+                file_size,
+                type
+              });
+            } else {
+              console.log('Recipient socket not found');
+            }
           }
-        }  
-        catch (messageError) {
-          console.error('Error saving message to MongoDB:', messageError);
-        }
-     
-    });
+          catch (messageError) {
+            console.error('Error saving message to MongoDB:', messageError);
+          }
 
-    socket.on(SocketEvents.GetMessages, async (data: { withUser: string }) => {
-      // console.log('getMessage', data);
-      const { withUser } = data;
-    
-      try {
-        // Retrieve messages from MongoDB
-        const mongoDBMessages = await Message.find({
-          $or: [
-            { from: userId, to: withUser },
-            { from: withUser, to: userId },
-          ],
-        })
-          .sort({ timestamp: 1 })
-          .skip(0)
-          .limit(10)
-          .exec();
-    
-        // Retrieve messages from Redis List
-        const redisMessages = await redisClient.lRange(`messages:${userId}:${withUser}`, 0, -1);
-    
-        // Parse Redis messages
-        const parsedRedisMessages = redisMessages.map((messageString) => JSON.parse(messageString));
-    
-        // Combine MongoDB and Redis messages
-        const allMessages = [...mongoDBMessages, ...parsedRedisMessages];
-    
-        socket.emit(SocketEvents.MessageHistory, { withUser, messages: allMessages });
-      } catch (error) {
-        console.error('Error fetching messages:', error);
-        socket.emit(SocketEvents.MessageHistory, { withUser, messages: [] });
-      }
-    });
-    
+        });
+
+      socket.on(SocketEvents.GetMessages, async (data: { withUser: string }) => {
+        // console.log('getMessage', data);
+        const { withUser } = data;
+
+        try {
+          // Retrieve messages from MongoDB
+          const mongoDBMessages = await Message.find({
+            $or: [
+              { from: userId, to: withUser },
+              { from: withUser, to: userId },
+            ],
+          })
+            .sort({ timestamp: 1 })
+            .skip(0)
+            .limit(10)
+            .exec();
+
+          // Retrieve messages from Redis List
+          const redisMessages = await redisClient.lRange(`messages:${userId}:${withUser}`, 0, -1);
+
+          // Parse Redis messages
+          const parsedRedisMessages = redisMessages.map((messageString) => JSON.parse(messageString));
+
+          // Combine MongoDB and Redis messages
+          const allMessages = [...mongoDBMessages, ...parsedRedisMessages];
+
+          socket.emit(SocketEvents.MessageHistory, { withUser, messages: allMessages });
+        } catch (error) {
+          console.error('Error fetching messages:', error);
+          socket.emit(SocketEvents.MessageHistory, { withUser, messages: [] });
+        }
+      });
+
     } catch (error) {
       console.error('JWT verification failed:', error);
       socket.disconnect();
--- src/models/Message.ts
+++ src/models/Message.ts
@@ -11,6 +11,8 @@ interface MessageAttributes {
   from: string;
   to: string;
   content: string;
+  file_name: string;
+  file_size: string;
   isRead?: boolean;
   type: MessageType;
   timestamp?: Date;
@@ -22,7 +24,9 @@ const messageSchema = new Schema<MessageDocument>({
   from: { type: String, required: true },
   to: { type: String, required: true },
   content: { type: String, required: true },
-  isRead: {type: Boolean, default: false},
+  isRead: { type: Boolean, default: false },
+  file_name: { type: String, required: false},
+  file_size: { type: String, required: false },
   type: {type: String, enum: Object.values(MessageType),default: MessageType.TEXT },
   timestamp: { type: Date, default: Date.now },
 });
